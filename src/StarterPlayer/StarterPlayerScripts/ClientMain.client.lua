1128532badef75f064d534c07af5705b5e084922
wallissonghost-code <wallissonghost@gmail.com>
2026-08-21 11:27:18 -0300

    feat: start business economy controller

--- src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
@@ -3,7 +3,9 @@ local Players = game:GetService("Players")
 local localPlayer = Players.LocalPlayer
 local BuildController = require(script.Parent:WaitForChild("BuildController"))
 local BusinessController = require(script.Parent:WaitForChild("BusinessController"))
+local BusinessEconomyController = require(script.Parent:WaitForChild("BusinessEconomyController"))
 
 BuildController:Start()
 BusinessController:Start()
+BusinessEconomyController:Start()
 print(string.format("[Property Empire v2] Client started for %s", localPlayer.Name))
