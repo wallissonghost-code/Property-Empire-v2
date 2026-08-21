37aad8633db5a0f6cc3f1279b36a2ca17a60fa55
wallissonghost-code <wallissonghost@gmail.com>
2026-08-21 13:46:40 -0300

    fix: isolate client controller startup

--- src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
@@ -1,11 +1,32 @@
 local Players = game:GetService("Players")
 
 local localPlayer = Players.LocalPlayer
-local BuildController = require(script.Parent:WaitForChild("BuildController"))
-local BusinessController = require(script.Parent:WaitForChild("BusinessController"))
-local BusinessEconomyController = require(script.Parent:WaitForChild("BusinessEconomyController"))
 
-BuildController:Start()
-BusinessController:Start()
-BusinessEconomyController:Start()
-print(string.format("[Property Empire v2] Client started for %s", localPlayer.Name))
+local function startController(moduleName)
+	task.spawn(function()
+		local moduleScript = script.Parent:WaitForChild(moduleName, 30)
+		if not moduleScript then
+			warn(string.format("[Property Empire v2] Client module %s was not found", moduleName))
+			return
+		end
+
+		local requireOk, controller = pcall(require, moduleScript)
+		if not requireOk then
+			warn(string.format("[Property Empire v2] Failed to require %s: %s", moduleName, tostring(controller)))
+			return
+		end
+
+		local startOk, startError = pcall(function()
+			controller:Start()
+		end)
+		if not startOk then
+			warn(string.format("[Property Empire v2] Failed to start %s: %s", moduleName, tostring(startError)))
+		end
+	end)
+end
+
+startController("BuildController")
+startController("BusinessController")
+startController("BusinessEconomyController")
+
+print(string.format("[Property Empire v2] Client bootstrap started for %s", localPlayer.Name))
